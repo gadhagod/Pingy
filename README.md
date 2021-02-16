44bc8d3d6152162c47c20948db502f7f06e1f077
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-02-16 14:47:09 -0800

    Not notifies when WiFi is back up after going down

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Be notified when your wifi goes down. Macs only.
 
-Simply run `nohup sh main.sh &`. If your wifi goes down, you'll be notified!
+Simply run `nohup sh main.sh &`. If your wifi goes down, you'll be notified! When it gets back up, you will be notified again.
--- main.sh
+++ main.sh
@@ -1,8 +1,15 @@
 while :; do
     ping -c 1 google.com
     if [[ $? != 0 ]]; then
-        osascript -e 'display notification "WiFi is down" with title "Request timout"'
-        sleep 120
+        osascript -e 'display notification "WiFi is down" with title "Request timeout"'
+        while :; do
+            ping -c 1 google.com
+            if [[ $? == 0 ]]; then
+                osascript -e 'display notification "WiFi is back up" with title "Request successful"'
+                break
+            fi
+            sleep 1
+        done
     fi
     sleep 1
 done
