848c9873d3d6627181cc55d987e4e8d961f542a2
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-02-16 11:13:48 -0800

    Enchance error notif

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 while :; do
     ping -c 1 google.com
     if [[ $? != 0 ]]; then
-        osascript -e 'display notification "WIFI IS DOWN!"'
+        osascript -e 'display notification "WiFi is down" with title "Request timout"'
         sleep 120
     fi
     sleep 1
