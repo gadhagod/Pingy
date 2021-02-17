04643695e0f99f35adad26b3decc1d1496e98fdf
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-02-17 13:41:40 -0800

    Add log ability

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Be notified when your wifi goes down. Macs only.
 
 Simply run `nohup sh main.sh &`. If your wifi goes down, you'll be notified! When it gets back up, you will be notified again.
+
+You can log times when your internet went down and came back up by passing an argument of the target log file. For example, you could run `sh main.sh ./log.out`. The following is an example log file:
+
+    Request timeout at Wed Feb 17 13:33:36 PST 2021
+    Request successful again at Wed Feb 17 13:33:50 PST 2021
+
+    Request timeout at Wed Feb 17 13:34:16 PST 2021
+    Request successful again at Wed Feb 17 13:34:27 PST 2021
--- main.sh
+++ main.sh
@@ -1,10 +1,18 @@
+logfile=$1
+
 while :; do
     ping -c 1 google.com
     if [[ $? != 0 ]]; then
         osascript -e 'display notification "WiFi is down" with title "Request timeout"'
+        if [ -n "$logfile" ]; then
+            echo "Request timeout at `date`" >> "$logfile"
+        fi
         while :; do
             ping -c 1 google.com
             if [[ $? == 0 ]]; then
+                if [ -n "$logfile" ]; then
+                    echo "Request successful again at `date`\n" >> "$logfile"
+                fi
                 osascript -e 'display notification "WiFi is back up" with title "Request successful"'
                 break
             fi
