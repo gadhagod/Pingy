3e5ef159a1d65ae413a7a004a8f7b86cb36c5854
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-04 12:18:53 -0700

    Make sure wifi is actually down, not temporary timeout

--- pingy.sh
+++ pingy.sh
@@ -10,21 +10,25 @@ sleep 5
 while :; do
     ping -c 1 google.com
     if [[ $? != 0 ]]; then
-        osascript -e 'display notification "WiFi is down" with title "Request timeout"'
-        if [ -n "$logfile" ]; then
-            echo "WiFi down at `date`" >> "$logfile"
-        fi
-        while :; do
-            ping -c 1 google.com
-            if [[ $? == 0 ]]; then
-                if [ -n "$logfile" ]; then
-                    echo "WiFi up at `date`\n" >> "$logfile"
-                fi
-                osascript -e 'display notification "WiFi is back up" with title "Request successful"'
-                break
+        sleep 2
+        ping -c 1 google.com
+        if [[ $? != 0 ]]; then
+            osascript -e 'display notification "WiFi is down" with title "Request timeout"'
+            if [ -n "$logfile" ]; then
+                echo "WiFi down at `date`" >> "$logfile"
             fi
-            sleep 1
-        done
+            while :; do
+                ping -c 1 google.com
+                if [[ $? == 0 ]]; then
+                    if [ -n "$logfile" ]; then
+                        echo "WiFi up at `date`\n" >> "$logfile"
+                    fi
+                    osascript -e 'display notification "WiFi is back up" with title "Request successful"'
+                    break
+                fi
+                sleep 1
+            done
+        fi
     fi
     sleep 1
 done
